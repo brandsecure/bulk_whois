db7cd90e3fac46d2422ddc96c8ab0a156fa9556c
brandsecure <129802619+brandsecure@users.noreply.github.com>
2023-04-03 16:50:57 -0600

    Update bulk_whois.py
    Update comments

--- bulk_whois.py
+++ bulk_whois.py
@@ -1,31 +1,40 @@
+# Import required libraries
 import os
 import csv
 import whois
 from datetime import datetime
 
-# Name output file and append datestamp
+# Generate a timestamp for the output file name
 timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
-output_file = f'bulk_whois_results_{timestamp}.csv'
+output_file = f'output_{timestamp}.csv'
 
-# Specify the name/location of the text file containing a list of domains
+# Read the list of domains from the 'domains.txt' file
 with open('domains.txt', 'r') as file:
     domains = [line.strip() for line in file.readlines()]
 
-# Add Columns for Domain and Registrar to the output file
+# Open the output CSV file for writing
 with open(output_file, 'w', newline='') as csvfile:
+    # Define the CSV field names
     fieldnames = ['Domain', 'Registrar']
+    
+    # Create a CSV writer object and write the header row
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
     writer.writeheader()
-# Loop through the domains and perform the WHOIS lookup
+
+    # Iterate through the list of domains
     for domain in domains:
         print("Processing:", domain)
         try:
+            # Perform a WHOIS query on the domain
             w = whois.whois(domain)
             registrar = w.registrar
+            
+            # Write the domain and registrar information to the CSV file
             if registrar is not None:
                 writer.writerow({'Domain': domain, 'Registrar': registrar})
             else:
-                writer.writerow({'Domain': domain, 'Registrar': 'ERROR: Unable to query whois database'})
+                writer.writerow({'Domain': domain, 'Registrar': 'ERROR'})
         except Exception as e:
+            # Handle exceptions and write an error entry to the CSV file
             print(f"Error processing {domain}: {e}")
             writer.writerow({'Domain': domain, 'Registrar': 'ERROR'})
