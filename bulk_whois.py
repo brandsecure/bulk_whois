602d71b50efc14ecfc1602d1c72a5ee2780b3ec5
brandsecure <129802619+brandsecure@users.noreply.github.com>
2023-04-03 20:54:36 -0600

    Add Registrar Abuse Email field

--- bulk_whois.py
+++ bulk_whois.py
@@ -1,40 +1,44 @@
-# Import required libraries
 import os
 import csv
 import whois
 from datetime import datetime
 
-# Generate a timestamp for the output file name
+def find_registrar_abuse_email(raw_data):
+    if not raw_data:
+        return None
+    lines = raw_data.split('\n')
+    for line in lines:
+        if 'Registrar Abuse Contact Email:' in line:
+            return line.split(': ')[1].strip()
+    return None
+
 timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
 output_file = f'output_{timestamp}.csv'
 
-# Read the list of domains from the 'domains.txt' file
 with open('domains.txt', 'r') as file:
     domains = [line.strip() for line in file.readlines()]
 
-# Open the output CSV file for writing
 with open(output_file, 'w', newline='') as csvfile:
-    # Define the CSV field names
-    fieldnames = ['Domain', 'Registrar']
-    
-    # Create a CSV writer object and write the header row
+    fieldnames = ['Domain', 'Registrar', 'Registrar Abuse Email']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
     writer.writeheader()
 
-    # Iterate through the list of domains
     for domain in domains:
         print("Processing:", domain)
         try:
-            # Perform a WHOIS query on the domain
             w = whois.whois(domain)
             registrar = w.registrar
-            
-            # Write the domain and registrar information to the CSV file
-            if registrar is not None:
-                writer.writerow({'Domain': domain, 'Registrar': registrar})
-            else:
-                writer.writerow({'Domain': domain, 'Registrar': '--ERROR QUERYING WHOIS SERVER--'})
+            registrar_abuse_email = find_registrar_abuse_email(w.text)
+
+            writer.writerow({
+                'Domain': domain,
+                'Registrar': registrar if registrar else '--ERROR QUERYING WHOIS SERVER--',
+                'Registrar Abuse Email': registrar_abuse_email if registrar_abuse_email else '--ERROR QUERYING WHOIS SERVER--',
+            })
         except Exception as e:
-            # Handle exceptions and write an error entry to the CSV file
             print(f"Error processing {domain}: {e}")
-            writer.writerow({'Domain': domain, 'Registrar': '--ERROR QUERYING WHOIS SERVER--'})
+            writer.writerow({
+                'Domain': domain,
+                'Registrar': '--ERROR QUERYING WHOIS SERVER--',
+                'Registrar Abuse Email': '--ERROR QUERYING WHOIS SERVER--',
+            })
